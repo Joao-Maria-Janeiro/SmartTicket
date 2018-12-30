e0d190e0ed6e3e7a0823aa579d0fc9cb8a65f10e
João Maria Janeiro <joaomariajaneiro1@gmail.com>
2018-12-30 21:07:02 +0000

    Added second screen functionality

--- mobileApp/lib/pages/services.dart
+++ mobileApp/lib/pages/services.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
+
+
+class Ticket {
+  int yourTicket;
+  String serviceName;
+  Ticket(String serviceName, int yourTicket){
+    this.yourTicket = yourTicket;
+    this.serviceName = serviceName;
+  }
+}
 
 class ServicePage extends StatefulWidget {
 
@@ -12,9 +25,70 @@ class ServicePage extends StatefulWidget {
 }
 
 class ServicePageState extends State<ServicePage> {
-  Set<String> clickedServices;
+  final String url = "https://smartticket.herokuapp.com/services";
+  List data;
+  List data2;
+  List filtered = new List();
+  Timer timer;
+  List tickets;
 
+  Future<String> getData() async {
+    var res = await http
+        .get(Uri.encodeFull(url), headers: {"Accept": "application/json", "key": "RANDOM KEY"});
 
+    setState(() {
+      var resBody = json.decode(res.body);
+      data = resBody;
+    });
+    filtered.clear();
+    filterData();
+
+    return "Success!";
+  }
+
+  void filterData(){
+    for (var i = 0; i < data.length; i++){
+      if (widget.clickedServices.contains(data[i]["name"])){
+        filtered.add(data[i]);
+      }
+    }
+    getTickets();
+  }
+
+  Future<String> getTickets() async {
+    if (tickets == null){
+      tickets = new List();
+      for (var i = 0; i < filtered.length; i++){
+        String urlz = "https://smartticket.herokuapp.com/take/" + filtered[i]["name"];
+        var res = await http.get(Uri.encodeFull(urlz), headers: {"Accept": "application/json", "key": "RANDOM KEY"});
+        var resBody = json.decode(res.body);
+        var yourTicketValue = resBody["ticket"];
+        tickets.add(yourTicketValue);
+      }
+    }
+    print(tickets);
+    return "ERROR";
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    timer = Timer.periodic(Duration(seconds: 1), (Timer t) => getData());
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
+
+  int getIndex() {
+    if (filtered == null || tickets == null){
+      return 0;
+    }else{
+      return tickets.length;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,6 +115,7 @@ class ServicePageState extends State<ServicePage> {
         appBar: new AppBar(
           title: new Text("Your Selected Services"),
         ),
+<<<<<<< Updated upstream
           body: new Container(
             margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
             height: 50.0,
@@ -51,6 +126,42 @@ class ServicePageState extends State<ServicePage> {
                     fontWeight: FontWeight.bold,
                     fontFamily: 'Montserrat')),
           )
+=======
+        body: ListView.builder(
+          itemCount: getIndex(),
+          itemBuilder: (BuildContext context, int index){
+            return new Container(
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Card(
+                      child: Container(
+                          padding: EdgeInsets.all(20.0),
+                          child: Column(
+                            children: <Widget>[
+                              Text("Name: "),
+                              Text(filtered[index]["name"],
+                                  style: TextStyle(
+                                      fontSize: 18.0, color: Colors.black87)),
+                              Text("Current Ticket: "),
+                              Text((filtered[index]["current_ticket"]).toString(),
+                                  style: TextStyle(
+                                      fontSize: 18.0, color: Colors.red)),
+                              Text("Your Ticket: "),
+                              Text((tickets[index]).toString(),
+                                  style: TextStyle(
+                                      fontSize: 18.0, color: Colors.black87)),
+                            ],
+                          )),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+>>>>>>> Stashed changes
       );
     }
   }
